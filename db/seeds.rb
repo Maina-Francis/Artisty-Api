d9a8558af27dbd951bad3e33ab4549321b0ee672
Francis Maina <sebastianfrancix@gmail.com>
2022-12-07 21:41:06 +0300

    updated the seed file to include sample image link

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,8 @@ end
         description: Faker::Lorem.sentence(word_count: 5),
         likes: rand(1..50),
         user_id: rand(1..10),
-        admin_id: rand(1..10)
+        admin_id: rand(1..10),
+        images: "https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
     )
 end
 puts "Done Seeding Data..."
