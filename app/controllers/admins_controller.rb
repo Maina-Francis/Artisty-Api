4cad19388bd6b0f1743f9bc6538eec2081a33a01
Francis Maina <sebastianfrancix@gmail.com>
2022-12-07 17:18:04 +0300

    added admin controllers

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,17 @@
 class AdminsController < ApplicationController
+    def index 
+        users = Admin.all 
+        render json: users
+    end
+
+    def create
+        admin = Admin.create!(admin_params)
+        render json: admin
+    end
+
+    #private methods
+    private 
+    def admin_params
+        params.permit(:name)
+    end
 end
