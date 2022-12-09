f64521bd8c1a6d1bf89dad427b50050d14a88597
Dennis <dennisgachomo@gmail.com>
2022-12-09 04:59:26 +0300

    signup created

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,5 @@
 class AdminsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_user_details
 
     def index 
         users = Admin.all 
@@ -10,10 +11,25 @@ class AdminsController < ApplicationController
         render json: admin
     end
 
+    def signup
+        admin = Admin.create!(params.permit(:first_name, :last_name, :email, :password))
+        render json: admin
+    end
+
+    def login
+        admin = find_bu
+    end
+
+
+
     #private methods
     private 
     def admin_params
         params.permit(:name)
+    end
+
+    def invalid_user_details
+        rendr json: {error: "invalid details"} status: :404
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :admins, only: [:index]
   resources :posts
   resources :users, only: [:index, :create, :show, :destroy]
- post "/signup", to: "users#signup"
+ post "/signup", to: "admins#signup"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
